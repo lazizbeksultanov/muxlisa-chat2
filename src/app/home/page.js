19f63950e62de5f53c0550323dc2e56daa87a302
lazizbeksultonov <lazizbeksultonov0804@gmail.com>
2023-05-03 08:51:57 +0500

    mini changes

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -3,15 +3,18 @@
 import FakeData from "@/components/fakeData";
 import {Providers} from "@/store/Providers";
 import VoiceRecorder from "@/components/VoiceRecorder";
+import {useGetUsersQuery} from "@/services/UserService";
 
 export default function Home() {
+
+    const {data, error, isLoading} = useGetUsersQuery()
+    console.log(data)
 
     return (
         <main className="h-full bg-white">
             <div className={"h-full relative"}>
                 <div>
-                    <FakeData/>
-                    <h1>HelloHelloHello</h1>
+                    {isLoading ? "Loading..." : data.map(item => <div key={item?.id}>{item?.name}</div>)}
                 </div>
                 <div
                     className="fixed max-w-[50%] bottom-0 right-0 left-0 lg:ml-32 sm:mx-6 lg:mr-8 mb-4 translate-x-1/2">
--- src/components/fakeData.js
+++ src/components/fakeData.js
@@ -1,11 +1,8 @@
 "use client"
 
 import { useDispatch, useSelector } from "react-redux";
-import { useGetUsersQuery } from "@/services/UserService";
 
 function FakeData () {
-    const {data} = useGetUsersQuery()
-    console.log(data)
     // const {value} = useSelector(state => state.counterReducer)
     //
     // console.log(value)
